022ac20c5197fe31649764198d9f43c8e25f6feb
chaiyikang <chaiyikang52@gmail.com>
2023-11-19 12:47:34 +0800

    fixed scrolling for todolist

--- src/MajorComponents/ToDoList.jsx
+++ src/MajorComponents/ToDoList.jsx
@@ -38,31 +38,29 @@ export function ToDoList({ toDos, setToDos, toDoIsOpen }) {
 	if (!toDoIsOpen) return;
 
 	return (
-		<div className=" w-70 absolute right-0 top-0 min-h-full bg-slate-900 p-4 opacity-75">
-			<div className="mx-auto p-4">
-				<h1 className="text-center text-4xl font-bold">To Do List</h1>
-				<div className="mt-4 flex items-center justify-center">
-					<input
-						type="text"
-						value={input}
-						onChange={handleNewTaskInputChange}
-						onKeyDown={handleEnter}
-						placeholder="Enter a new task"
-						className="h-7 w-auto rounded-xl border border-slate-400 bg-transparent px-4 text-center text-xl focus:border-2 focus:outline-none"
-					/>
-					<Button onClick={handleAddTask} additionalClassName="ml-4">
-						Add
-					</Button>
-				</div>
-				<ul className="mt-4">
-					{toDos.map((task, index) => (
-						<ToDo task={task} index={index} key={index} toDos={toDos} setToDos={setToDos} />
-					))}
-				</ul>
-				<Button onClick={handleClear} additionalClassName="mx-14">
-					Clear
+		<div className=" w-70 absolute right-0 top-0 h-screen overflow-y-auto  bg-slate-900 p-4 opacity-75">
+			<h1 className="text-center text-4xl font-bold">To Do List</h1>
+			<div className="mt-4 flex items-center justify-center">
+				<input
+					type="text"
+					value={input}
+					onChange={handleNewTaskInputChange}
+					onKeyDown={handleEnter}
+					placeholder="Enter a new task"
+					className="h-7 w-auto rounded-xl border border-slate-400 bg-transparent px-4 text-center text-xl focus:border-2 focus:outline-none"
+				/>
+				<Button onClick={handleAddTask} additionalClassName="ml-4">
+					Add
 				</Button>
 			</div>
+			<ul className=" mt-4 ">
+				{toDos.map((task, index) => (
+					<ToDo task={task} index={index} key={index} toDos={toDos} setToDos={setToDos} />
+				))}
+			</ul>
+			<Button onClick={handleClear} additionalClassName="mx-14">
+				Clear
+			</Button>
 		</div>
 	);
 }
