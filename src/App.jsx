b2943c5b6a985b8e130c7ce765fe3e6811d51b39
chaiyikang <chaiyikang52@gmail.com>
2023-12-04 16:27:00 +0800

    updated update msg

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,7 @@ import PageLoadingSpinner from "./MajorComponents/PageLoadingSpinner.jsx";
 import AccountModal from "./MajorComponents/AccountModal.jsx";
 import Cat from "./MajorComponents/Cat.jsx";
 
-const updateMessage = `30 Nov 2023 Updates: 
-1. Lifetime stats like total time focused and total sessions are permanent and will no longer be cleared.
-2. Clearing stats will only remove the timeline and not the lifetime stats.
-3. Stats were improved to reflect hours and minutes instead of seconds.
-4. Height of timeline blocks has been scaled down.
-5. To-dos can now be individually deleted
-
+const updateMessage = `
 1 Dec 2023 Updates:
 1. Toggling components (Stats, Pomodoro, To-do List) now has animations
 
