c831b16607f98805d1b87121a0008d7a2c982c46
chaiyikang <chaiyikang52@gmail.com>
2023-12-30 14:48:32 +0800

    edited color of page loading spinner and edited errorboundary message

--- src/MiscComponents/PageLoadingSpinner.jsx
+++ src/MiscComponents/PageLoadingSpinner.jsx
@@ -1,13 +1,18 @@
-import { useContext } from "react";
-import { ThemeContext } from "../features/Theming/ThemeContext";
+import { getInitThemeByTime } from "../main/helpers";
 
 function PageLoadingSpinner() {
-	const { themeColour } = useContext(ThemeContext);
+	const day = getInitThemeByTime() === "seoulInsideDay" ? true : false;
 
 	return (
-		<div className={`grid h-screen w-screen place-items-center bg-neutral-800`}>
+		<div
+			className={`grid h-screen w-screen place-items-center ${
+				day ? "bg-amber-100" : "bg-indigo-700"
+			}`}
+		>
 			<div
-				className={`text-netural-400 inline-block h-[10rem] w-[10rem] animate-spin rounded-full border-[1rem] border-current border-t-transparent`}
+				className={`${
+					day ? "text-amber-600" : "text-indigo-200"
+				} inline-block h-[10rem] w-[10rem] animate-spin rounded-full border-[1rem] border-current border-t-transparent`}
 				role="status"
 				aria-label="loading"
 			>
--- src/features/ErrorBoundary/ErrorFallback.jsx
+++ src/features/ErrorBoundary/ErrorFallback.jsx
@@ -2,7 +2,8 @@ function ErrorFallback({ error }) {
 	return (
 		<>
 			<div className="grid h-screen w-screen place-items-center bg-slate-900 text-5xl text-slate-400">
-				There was an error. The error message is: &quot;{error.message}&quot;
+				There was an error. Please try using a desktop, updating your browser, or using a browser
+				like Chrome. The error message is: &quot;{error.message}&quot;
 			</div>
 		</>
 	);
